1dd27fdff3b07c24db0648e57ebdb229b3401ee8
Jan Dryk <me@uxsoft.cz>
2026-03-27 23:38:33 +0100

    fix radio button

--- src/components/radio_group.rs
+++ src/components/radio_group.rs
@@ -36,9 +36,8 @@ pub fn RadioGroupItem(
     #[props(default)] class: String,
     #[props(default)] id: Option<String>,
 ) -> Element {
-    let ctx = use_context::<Signal<RadioGroupContext>>();
-    let ctx_read = ctx.read();
-    let is_checked = ctx_read.value == value;
+    let mut ctx = use_context::<Signal<RadioGroupContext>>();
+    let is_checked = ctx.read().value == value;
 
     let classes = cn(&[
         "aspect-square size-4 shrink-0 rounded-full border border-input text-primary shadow-xs transition-[color,box-shadow] outline-none cursor-pointer",
@@ -47,8 +46,8 @@ pub fn RadioGroupItem(
         "dark:bg-input/30",
         &class,
     ]);
-    let onchange_handler = ctx_read.onchange.clone();
     let value_clone = value.clone();
+    let dot_class = if is_checked { "scale-100 opacity-100" } else { "scale-0 opacity-0" };
 
     rsx! {
         button {
@@ -59,22 +58,22 @@ pub fn RadioGroupItem(
             disabled,
             "aria-checked": if is_checked { "true" } else { "false" },
             onclick: move |_| {
-                if let Some(handler) = &onchange_handler {
+                let onchange = ctx.read().onchange.clone();
+                ctx.write().value = value_clone.clone();
+                if let Some(handler) = onchange {
                     handler.call(value_clone.clone());
                 }
             },
-            if is_checked {
-                span {
-                    class: "flex items-center justify-center",
-                    svg {
-                        class: "size-2 fill-primary",
-                        xmlns: "http://www.w3.org/2000/svg",
-                        view_box: "0 0 24 24",
-                        circle {
-                            cx: "12",
-                            cy: "12",
-                            r: "12",
-                        }
+            span {
+                class: "flex items-center justify-center",
+                svg {
+                    class: "size-2 fill-primary transition-[transform,opacity] duration-150 ease-out {dot_class}",
+                    xmlns: "http://www.w3.org/2000/svg",
+                    view_box: "0 0 24 24",
+                    circle {
+                        cx: "12",
+                        cy: "12",
+                        r: "12",
                     }
                 }
             }
